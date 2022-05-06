8a6b688b71347b68827d6159ef6aebc2d19fe777
Jason Carvalho <jason@20c.com>
2022-05-06 11:42:26 +0530

    remove duplicate line

--- tests/project/settings.py
+++ tests/project/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django_otp.middleware.OTPMiddleware",
 ]
 
